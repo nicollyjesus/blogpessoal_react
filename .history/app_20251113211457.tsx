6277639eef6d220fcbbe2983fe095fb4dceda555
nicollyjesus <nicolly.jesus.nj@gmail.com>
2025-11-13 21:17:11 -0300

    :lipstick: Ajuste na estilização

---
+++ .history/app_20251113211457.tsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Footer from "./compo"
+import Navbar from "./components/navbar/Navbar"
+import Home from "./pages/home/Home"
+import Login from "./pages/login/Login"
+import Cadastro from "./pages/cadastro/Cadastro"
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
